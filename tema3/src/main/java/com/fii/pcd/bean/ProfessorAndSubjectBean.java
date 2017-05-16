765601e6b8c079fdcc63f960e1749c5576dc3cea
Ervin <ervin.necula@info.uaic.ro>
2017-05-16 09:04:27 +0300

    Added view bean for UI

---
+++ tema3/src/main/java/com/fii/pcd/bean/ProfessorAndSubjectBean.java
@@ -0,0 +1,24 @@
+package com.fii.pcd.bean;
+
+public class ProfessorAndSubjectBean {
+
+    private String professorName;
+
+    private String subjectName;
+
+    public String getProfessorName() {
+        return professorName;
+    }
+
+    public void setProfessorName(String professorName) {
+        this.professorName = professorName;
+    }
+
+    public String getSubjectName() {
+        return subjectName;
+    }
+
+    public void setSubjectName(String subjectName) {
+        this.subjectName = subjectName;
+    }
+}
